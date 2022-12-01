f54a82b0c968fa642ad3dfd6fd11e1e8a35ae383
huuhkam <huuhkam@dnb.com>
2022-12-01 15:49:24 +0200

    feat: data fetcher

--- src/main/java/com/huuhkisowl/graphql/controller/MovieController.java
+++ src/main/java/com/huuhkisowl/graphql/controller/MovieController.java
@@ -2,6 +2,7 @@ package com.huuhkisowl.graphql.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -13,25 +14,54 @@ import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
+import com.huuhkisowl.graphql.model.Actor;
+import com.huuhkisowl.graphql.model.Director;
+import com.huuhkisowl.graphql.model.Genre;
 import com.huuhkisowl.graphql.model.Movie;
+import com.huuhkisowl.graphql.repository.ActorRepository;
+import com.huuhkisowl.graphql.repository.DirectorRepository;
+import com.huuhkisowl.graphql.repository.GenreRepository;
 import com.huuhkisowl.graphql.repository.MovieRepository;
 
 @Controller
 public class MovieController {
 
 	private final MovieRepository movieRepository;
+	private final GenreRepository genreRepository;
+	private final ActorRepository actorRepository;
+	private final DirectorRepository directorRepository;
 	
 	private final MongoTemplate mongoTemplate;
 	
 	@Autowired
-	public MovieController(MovieRepository movieRepository, MongoTemplate mongoTemplate) {
+	public MovieController(MovieRepository movieRepository, ActorRepository actorRepository,
+			GenreRepository genreRepository, DirectorRepository directorRepository, MongoTemplate mongoTemplate) {
         this.movieRepository = movieRepository;
+        this.actorRepository = actorRepository;
+        this.genreRepository = genreRepository;
+        this.directorRepository = directorRepository;
         this.mongoTemplate = mongoTemplate;
     }
 	
 	@SchemaMapping(typeName = "Query", value = "movies")
     public List<Movie> findAll() {
         return movieRepository.findAll();
+    }
+	
+	@SchemaMapping
+    public List<Genre> genres(Movie movie) {
+        return genreRepository.findByIdIn(movie.getGenresIds());
+    }
+	
+	@SchemaMapping
+    public List<Actor> actors(Movie movie) {
+        return actorRepository.findByIdIn(movie.getActorsIds());
+    }
+	
+	@SchemaMapping
+    public Director director(Movie movie) {
+        Optional<Director> optionalEnitry = directorRepository.findById(movie.getDirectorId());
+		return optionalEnitry.get();
     }
 	
 	@QueryMapping
