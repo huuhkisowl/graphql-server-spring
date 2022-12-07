c1bbc170404b54f2475b12b8a384f9b47ab70a4c
huuhkam <huuhkam@dnb.com>
2022-12-07 23:20:22 +0200

    Find movie by actors, director or genres

--- src/main/java/com/huuhkisowl/graphql/repository/MovieRepository.java
+++ src/main/java/com/huuhkisowl/graphql/repository/MovieRepository.java
@@ -3,11 +3,21 @@ package com.huuhkisowl.graphql.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.huuhkisowl.graphql.model.Movie;
 
 public interface MovieRepository extends MongoRepository<Movie, String> {
 	
 	List<Movie> findByIdIn(List<String> ids);
+	
+	@Query("{ 'genresIds' : ?0 }")
+	List<Movie> findByGenreId(String id);
+	
+	@Query("{ 'actorsIds' : ?0 }")
+	List<Movie> findByActorId(String id);
+	
+	@Query("{ 'directorId' : ?0 }")
+	List<Movie> findByDirectorId(String id);
 
 }
